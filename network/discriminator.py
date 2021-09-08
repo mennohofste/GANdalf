262633bc83287995c715917e6833549d323b111d
Menno <menno1337@gmail.com>
2021-09-08 23:22:47 +0200

    add GC discriminator

--- network/discriminator.py
+++ network/discriminator.py
@@ -1,10 +1,41 @@
 import torch
 import torch.nn as nn
+from torch.nn.utils import spectral_norm
+
+
+class SNPatchDisc(nn.Module):
+    def __init__(self, nr_class=2):
+        super().__init__()
+        self.nr_class = nr_class
+
+        def block(in_c, out_c, stride=2):
+            return nn.Sequential(
+                spectral_norm(nn.Conv2d(in_c, out_c, 5, stride, 2)),
+                nn.LeakyReLU(0.2),
+            )
+
+        layers = []
+        layers.append(block(3, 64, 1))
+        layers.append(block(64, 128))
+        layers.append(block(128, 256))
+        layers.append(block(256, 256))
+        layers.append(block(256, 256))
+        layers.append(block(256, 256))
+        self.conv = nn.Sequential(*layers)
+
+        self.cls = nn.Conv2d(256, nr_class, 8)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.nr_class == 1:
+            return x.flatten(1)
+        return x.flatten(1), self.cls(x).squeeze()
 
 
 class StarDisc(nn.Module):
-    def __init__(self, img_size=256, nr_class=2):
+    def __init__(self, nr_class=2):
         super().__init__()
+        self.nr_class = nr_class
 
         def block(in_c, out_c):
             return nn.Sequential(
@@ -22,11 +53,20 @@ class StarDisc(nn.Module):
         self.conv = nn.Sequential(*layers)
 
         self.src = nn.Conv2d(2048, 1, 3, 1, 1)
-        self.cls = nn.Conv2d(2048, nr_class, img_size // 64)
+        self.cls = nn.Conv2d(2048, nr_class, 4)
 
     def forward(self, x):
-        features = self.conv(x)
-        return self.src(features), self.cls(features).squeeze()
+        x = self.conv(x)
+        if self.nr_class == 1:
+            return self.src(x)
+        return self.src(x), self.cls(x).squeeze()
+
+
+def test():
+    x = torch.randn((5, 3, 256, 256))
+    model = SNPatchDisc()
+    preds = model(x)
+    print(preds[0].shape, preds[1].shape)
 
 
 def test1():
@@ -37,4 +77,5 @@ def test1():
 
 
 if __name__ == "__main__":
+    test()
     test1()
