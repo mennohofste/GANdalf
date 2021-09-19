8c9d5c1c49787932bbedbd7873fcebe08b35d82a
Menno <menno1337@gmail.com>
2021-09-19 13:34:23 +0200

    add model checkpoint

--- train.py
+++ train.py
@@ -1,11 +1,21 @@
+#!/var/home/menno/Documents/GANdalf/.venv/bin/python
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+
 from network.architecture import CycleGAN
 from data import FaceDataModule
 
 
 def main():
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val/l1',
+        dirpath='checkpoints',
+        filename='CycleGAN-epoch{epoch:02d}-l1{val/l1:.2f}',
+        auto_insert_metric_name=False
+    )
+
     model = CycleGAN()
-    trainer = Trainer()
+    trainer = Trainer(callbacks=[checkpoint_callback])
     dm = FaceDataModule()
 
     trainer.fit(model, dm)
