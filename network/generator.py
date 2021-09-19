61b3288b2a44bd69b1419d6a08aa1fc259b0ef50
Menno <menno1337@gmail.com>
2021-09-19 13:34:46 +0200

    add only 1 class option in generator

--- network/generator.py
+++ network/generator.py
@@ -5,6 +5,8 @@ import torch.nn as nn
 class StarGen(nn.Module):
     def __init__(self, nr_class=2, nr_resblocks=6, upconv='deconv'):
         super().__init__()
+        if nr_class == 1:
+            nr_class = 0
 
         def block(in_c=256, out_c=256, kernel=3, stride=1, padding=1, conv_m='conv'):
             if conv_m == 'conv':
@@ -17,14 +19,15 @@ class StarGen(nn.Module):
                     nn.Conv2d(in_c, out_c, kernel, stride, padding),
                 )
             else:
-                raise Exception('Convolutional method', conv_m, 'not recognised!')
+                raise Exception('Convolutional method',
+                                conv_m, 'not recognised!')
 
             return nn.Sequential(
                 conv,
                 nn.InstanceNorm2d(out_c),
                 nn.ReLU(),
             )
-        
+
         layers = []
         layers.append(block(3 + nr_class, 64, 7, 1, 3))
         layers.append(block(64, 128, 4, 2, 1))
@@ -34,7 +37,7 @@ class StarGen(nn.Module):
         self.bottleneck = []
         for _ in range(nr_resblocks):
             self.bottleneck.append(block())
-            
+
         layers = []
         layers.append(block(256, 128, 4, 2, 1, upconv))
         layers.append(block(128, 64, 4, 2, 1, upconv))
@@ -49,12 +52,20 @@ class StarGen(nn.Module):
         return self.decoder(x)
 
 
-def test():
+def test0():
     x = torch.randn((7, 3 + 2, 256, 256))
     model = StarGen()
     preds = model(x)
     print(preds.shape)
 
 
+def test1():
+    x = torch.randn((7, 3, 256, 256))
+    model = StarGen(1)
+    preds = model(x)
+    print(preds.shape)
+
+
 if __name__ == "__main__":
-    test()
+    test0()
+    test1()
